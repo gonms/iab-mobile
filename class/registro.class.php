0c43b1b31a9535dd8d432161682c5acd78745d25
Gon <gonmunoz.wysiwyg@gmail.com>
2014-10-28 14:02:49 +0100

    cambiado mail

--- class/registro.class.php
+++ class/registro.class.php
@@ -11,6 +11,7 @@ class Registro{
         $email = "gon.munoz.sanchez@gmail.com"; //"comunicacion@iabspain.net";
         $cabeceras  = 'MIME-Version: 1.0' . "\r\n";
         $cabeceras .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+        $cabeceras .= 'Fro: IAB <comunicacion@iabspain.net>' . "\r\n";
         $cabeceras .= 'To: IAB <' . $email . '>' . "\r\n";
 
         // Enviarlo
@@ -18,13 +19,15 @@ class Registro{
                     <body>
                         <p>Se ha realizado el envío de estos datos a través de la web móvil:</p>
                         <p>Nombre: ' . $_POST['nombre'] . ' ' . $_POST['apellidos'] . '</p>
-                        <p>Empresa: ' . $_POST['noEmpresa'] . '</p>
+                        <p>Empresa: ' . $_POST['nomEmpresa'] . '</p>
                         <p>Email: ' . $_POST['email'] . '</p>
                     </body>
                     </html>
                     ');
 
-        mail($email, "Envio datos Asociate web móvil", $mensaje, $cabeceras);
+        mail($email, utf8_decode("Envío datos Asociate web móvil"), $mensaje, $cabeceras);
+
+        $aDatos[] = "Los datos se han enviado correctamente!";
 
 		return $aDatos;
 	}
